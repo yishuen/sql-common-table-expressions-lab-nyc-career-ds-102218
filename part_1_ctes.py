dd363c4d2d1f100311065579c31ea35b29d50f16
Jake <jakemacnaughton@gmail.com>
2018-05-14 14:19:44 -0400

    removed solutions

--- part_1_ctes.py
+++ part_1_ctes.py
@@ -1,13 +1,11 @@
 def use_cte_to_determine_average_sale():
-    return """WITH average_sales AS
-            (
-            SELECT AVG(amount) AS average FROM sales
-            )
-            SELECT * FROM average_sales;"""
+    return """Write
+            your
+            query
+            here"""
 
 def select_all_above_average_sales():
-    return """WITH average_sales AS
-            (
-            SELECT AVG(amount) AS average FROM sales
-            )
-            SELECT * FROM sales WHERE amount > (SELECT average FROM average_sales);"""
+    return """Write
+            your
+            query
+            here"""
